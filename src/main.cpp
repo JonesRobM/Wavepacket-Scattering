87241710d5741966c63e07c6012d6492c2edef18
JonesRobM <robert.m.jones@kcl.ac.uk>
2026-06-06 20:07:33 +0100

    Skeleton in place

---
+++ src/main.cpp
@@ -0,0 +1,6 @@
+/// Initialise -> Time iterations -> Stream to disk
+/// Step 1. Data structures and representations
+
+/// The spatial domain is mapped to a uniform 1d lattice of spacing J
+/// QM requires complex amplitudes. The entire state of the system at any given moment
+/// can be held in standard contiguous memory
